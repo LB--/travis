a562d0133399425f10b0064c622d97d53d2ae4b2
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-24 01:33:29 -0500

    First stab at clang

--- install-biicode.sh
+++ install-biicode.sh
@@ -5,6 +5,5 @@ mkdir -p ".travis/biicode" && cd ".travis"
 wget https://s3.amazonaws.com/biibinaries/release/3.3/bii-ubuntu-64_3_3.deb
 dpkg -x bii-ubuntu-64_3_3.deb ./biicode/
 cd biicode/opt/biicode
-ls -lah
 BIIPATH=$(pwd)
 export PATH="$BIIPATH:$PATH"
--- update-clang.sh
+++ update-clang.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# We build clang manually because I don't know a better way
+
+mkdir -p ".travis/clang" && cd ".travis"
+svn co -q http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd llvm/tools
+svn co -q http://llvm.org/svn/llvm-project/cfe/trunk clang
+cd ../projects
+svn co -q http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+svn co -q http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+cd ../..
+CLANGPATH=$(pwd)
+mkdir build && cd build
+cmake -G "Unix Makefiles" ../llvm/tools/clang -DCMAKE_INSTALL_PREFIX="$CLANGPATH/install" -DCMAKE_BUILD_TYPE=RELEASE
+make install
+cd ../install/usr/bin
+ls -lah
+CLANGPATH=$(pwd)
+export PATH="$CLANGPATH:$PATH"

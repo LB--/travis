b8d1672641110f04f463bb927a16700afb241af1
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-24 01:00:02 -0500

    Actually step into biicode folder

--- install-biicode.sh
+++ install-biicode.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # The official biicode installation script requires sudo, which isn't available on containers
 
-mkdir -p ".travis/biicode" && cd ".travis/biicode"
-BIIPATH=$(pwd)
+mkdir -p ".travis" && cd ".travis"
 git clone https://github.com/biicode/biicode.git
+cd biicode
+BIIPATH=$(pwd)
 git submodule update --init --recursive
 pip install -r client/requirements.txt
 pip install -r common/requirements.txt

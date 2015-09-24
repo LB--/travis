6cdd33eb8a25a184aa692c488dc99d14581d5bca
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-24 01:15:18 -0500

    See what I am up against

--- install-biicode.sh
+++ install-biicode.sh
@@ -5,5 +5,6 @@ mkdir -p ".travis/biicode" && cd ".travis"
 wget https://s3.amazonaws.com/biibinaries/release/3.3/bii-ubuntu-64_3_3.deb
 dpkg -x bii-ubuntu-64_3_3.deb ./biicode/
 cd biicode
+ls -lah
 BIIPATH=$(pwd)
 export PATH="$BIIPATH:$PATH"

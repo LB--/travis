7e1c210e2b99601bc7f709ef94f913575dbfba32
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 17:54:00 -0500

    Trying random things for cmake 3.3

--- update-cmake.sh
+++ update-cmake.sh
@@ -2,8 +2,8 @@
 # biicode can update cmake to 3.0.2, but we want 3.3 at least
 
 mkdir -p ".travis/cmake" && cd ".travis"
-wget http://ftp.us.debian.org/debian/pool/main/c/cmake/cmake_3.3.2-1_i386.deb
-dpkg -x cmake_3.3.2-1_i386.deb ./cmake/
+wget http://opensource.exmeritus.com/mirror/debian-ports//pool-hppa/main/c/cmake/cmake_3.3.2-1_hppa.deb
+dpkg -x cmake_3.3.2-1_hppa.deb ./cmake/
 # http://stackoverflow.com/a/501381/1959975 (temporary)
 find . -exec ls -dl \{\} \; | awk '{print $3, $4, $9}'
 cd cmake/usr/bin

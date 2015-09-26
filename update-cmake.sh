38210bd9c3d0343830baf1fce804ec383845620c
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 17:32:05 -0500

    Debugging cmake 3.3

--- update-cmake.sh
+++ update-cmake.sh
@@ -4,6 +4,8 @@
 mkdir -p ".travis/cmake" && cd ".travis"
 wget http://ftp.debian.org/debian/pool/main/c/cmake/cmake_3.3.2-1_arm64.deb
 dpkg -x cmake_3.3.2-1_arm64.deb ./cmake/
+# http://stackoverflow.com/a/501381/1959975 (temporary)
+find . -exec ls -dl \{\} \; | awk '{print $3, $4, $9}'
 cd cmake/usr/bin
 CMAKEPATH=$(pwd)
 export PATH="$CMAKEPATH:$PATH"

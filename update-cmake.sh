a0ad67636e9603a9ccf8919c4b5a66c39a5db9d7
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 18:46:53 -0500

    Try to cache cmake 3.3 to reduce build times

--- update-cmake.sh
+++ update-cmake.sh
@@ -2,11 +2,16 @@
 # biicode can update cmake to 3.0.2, but we want 3.3 at least
 # unfortunately the easiest way I can find to do this is to build it from source
 
-mkdir -p ".travis/cmake" && cd ".travis"
-git clone https://github.com/Kitware/CMake.git cmake
-cd cmake && mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install ..
-cmake --build . && cmake --build . --target install
-cd install/bin
+if [ ! -e ".travis/cmake/build/install" ];
+then
+	mkdir -p ".travis/cmake" && cd ".travis"
+	git clone https://github.com/Kitware/CMake.git cmake
+	cd cmake && mkdir build && cd build
+	cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install ..
+	cmake --build . && cmake --build . --target install
+	cd install/bin
+else
+	cd ".travis/cmake/install/bin"
+fi
 CMAKEPATH=$(pwd)
 export PATH="$CMAKEPATH:$PATH"

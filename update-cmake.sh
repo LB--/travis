35315d269343bc51e10aed1930032ba095ce723b
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 17:50:55 -0500

    Maybe try 32bit cmake 3.3

--- update-cmake.sh
+++ update-cmake.sh
@@ -2,8 +2,8 @@
 # biicode can update cmake to 3.0.2, but we want 3.3 at least
 
 mkdir -p ".travis/cmake" && cd ".travis"
-wget http://ftp.debian.org/debian/pool/main/c/cmake/cmake_3.3.2-1_arm64.deb
-dpkg -x cmake_3.3.2-1_arm64.deb ./cmake/
+wget http://ftp.us.debian.org/debian/pool/main/c/cmake/cmake_3.3.2-1_i386.deb
+dpkg -x cmake_3.3.2-1_i386.deb ./cmake/
 # http://stackoverflow.com/a/501381/1959975 (temporary)
 find . -exec ls -dl \{\} \; | awk '{print $3, $4, $9}'
 cd cmake/usr/bin

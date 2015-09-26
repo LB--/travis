3dc0158e84b2ff3af641e3c43849933c3722c28e
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 18:11:27 -0500

    Eh, let's just build it from scratch

--- update-cmake.sh
+++ update-cmake.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # biicode can update cmake to 3.0.2, but we want 3.3 at least
+# unfortunately the easiest way I can find to do this is to build it from source
 
 mkdir -p ".travis/cmake" && cd ".travis"
-wget http://ftp.debian.org/debian/pool/main/c/cmake/cmake_3.3.2-1_arm64.deb
-dpkg -x cmake_3.3.2-1_arm64.deb ./cmake/
-# http://stackoverflow.com/a/501381/1959975 (temporary)
-find . -exec ls -dl \{\} \; | awk '{print $3, $4, $9}'
-cd cmake/usr/bin
-chmod +x cmake && chmod +x ctest && chmod +x cpack
+git clone https://github.com/Kitware/CMake.git cmake
+cd cmake && mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install ..
+cmake --build . && cmake --build . --target install
+cd install/usr/bin
 CMAKEPATH=$(pwd)
 export PATH="$CMAKEPATH:$PATH"

a1af1c1580b7ec4f6b2f938267ba5d3716bab8d4
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 17:25:52 -0500

    Okay, what directory structure does it use?

--- update-cmake.sh
+++ update-cmake.sh
@@ -4,6 +4,6 @@
 mkdir -p ".travis/cmake" && cd ".travis"
 wget http://ftp.debian.org/debian/pool/main/c/cmake/cmake_3.3.2-1_arm64.deb
 dpkg -x cmake_3.3.2-1_arm64.deb ./cmake/
-cd cmake/bin
+cd cmake/ && ls -lah
 CMAKEPATH=$(pwd)
 export PATH="$CMAKEPATH:$PATH"

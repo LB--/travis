3a0549fe572aa789f8224d47544659bf811a74cc
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-09-26 23:24:30 -0500

    Perhaps the old 'I only want clang' trick still works

--- update-clang.sh
+++ update-clang.sh
@@ -2,6 +2,7 @@
 # We build clang manually because I don't know a better way
 
 if [ ! -d ".travis/clang/install/bin" ]; then
+	rm -rf ".travis/clang"
 	mkdir -p ".travis/clang" && cd ".travis/clang"
 	git clone https://github.com/llvm-mirror/llvm.git llvm
 	cd llvm/tools
@@ -14,7 +15,7 @@ if [ ! -d ".travis/clang/install/bin" ]; then
 	mkdir build && cd build
 	export CC=gcc-5
 	export CXX=g++-5
-	cmake ../llvm -DCMAKE_INSTALL_PREFIX="$CLANGPATH/install" -DCMAKE_BUILD_TYPE=RELEASE
+	cmake ../llvm/tools/clang -DCMAKE_INSTALL_PREFIX="$CLANGPATH/install" -DCMAKE_BUILD_TYPE=RELEASE
 	cmake --build . && cmake --build . --target install
 	cd ../install/bin
 else

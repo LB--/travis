8e28d7d6dcd787ecf4477bb27c0b2eb11358c844
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-10-01 00:12:25 -0500

    Attempt to build boost

---
+++ update-boost.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Builds the latest version of Boost
+
+if [ ! -d ".travis/boost-install/lib" ]; then
+	rm -rf ".travis/boost"
+	rm -rf ".travis/boost-install"
+	mkdir -p ".travis" && cd ".travis"
+	git clone --recursive https://github.com/boostorg/boost.git boost >/dev/null
+	cd boost && chmod +x bootstrap.sh
+	./bootstrap.sh --prefix="$(pwd)/../boost-install"
+	./b2 headers
+	# We have to build boost using GCC 5.1
+	# If you know how to build boost with Clang 3.8, fix this!
+	./b2 toolset=gcc-5 cxxflags="-std=c++14" variant=release link=static install -d0
+else
+	mkdir -p ".travis/boost" && cd ".travis/boost"
+fi
+cd ../boost-install
+export BOOST_ROOT="$(pwd)"

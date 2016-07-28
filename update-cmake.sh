0715fa06ccf35d201859f1d88532c5548c6a0799
Nicholas "LB" Braden <LB@LB-Stuff.com>
2016-07-28 14:37:23 -0500

    Update CMake to v3.6.1

--- update-cmake.sh
+++ update-cmake.sh
@@ -4,9 +4,9 @@
 if [ ! -d ".travis/cmake/build/install/bin" ]; then
 	rm -rf ".travis/cmake"
 	mkdir -p ".travis/cmake" && cd ".travis"
-	git clone --recursive --depth 1 --branch v3.5.2 https://github.com/Kitware/CMake.git cmake
+	git clone --recursive --depth 1 --branch v3.6.1 https://github.com/Kitware/CMake.git cmake
 	cd cmake && mkdir build && cd build
-	cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=RELEASE ..
+	cmake .. -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_USE_SYSTEM_CURL=ON
 	cmake --build . && cmake --build . --target install
 	cd install/bin
 else
